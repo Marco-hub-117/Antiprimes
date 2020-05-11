dbe2539209bee04f3e0f24432cc0aa6d0d2d572e
Marco-hub-117 <62067957+Marco-hub-117@users.noreply.github.com>
2020-05-11 16:05:06 +0200

    Creata classe number processor

---
+++ src/antiprimes/NumberProcessor.java
@@ -0,0 +1,5 @@
+package antiprimes;
+
+public class NumberProcessor {
+
+}
--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -7,12 +7,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
 
 
 /**
  * The application window.
  */
-public class MainWindow extends JFrame {
+@SuppressWarnings("deprecation")
+public class MainWindow extends JFrame implements Observer {
 
     private AntiPrimesSequence sequence;
     private DefaultListModel display = new DefaultListModel();
@@ -73,4 +76,15 @@ public class MainWindow extends JFrame {
         for (Number n : sequence.getLastK(SHOW_LAST))
             display.add(0, "" + n.getValue() + " (" + n.getDivisors() + ")");
     }
+
+	@Override
+	public void update(Observable o, Object arg) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				updateDisplay();	
+			}
+		});
+		
+	}
 }
